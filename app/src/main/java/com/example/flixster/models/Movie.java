9890d3e874207fa9599990a1946a940f2947f0ff
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-03-23 23:06:13 -0400

    Using Intent and Parcelable

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,29 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by the Parceler Library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
-        backdropPath = jsonObject.getString("backdrop_path");
+        backdropPath = jsonObject.getString( "backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -28,6 +35,7 @@ public class Movie {
         }
         return movies;
     }
+
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
@@ -41,7 +49,9 @@ public class Movie {
         return title;
     }
 
-    public String getOverview() {
-        return overview;
+    public String getOverview() { return overview; }
+
+    public double getRating() {
+        return rating;
     }
 }
